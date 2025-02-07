78b72ed8bf3b3528a6d01c3da6488da6b26747c2
Thiago Perin <thiagoperin2310@gmail.com>
2025-02-06 23:59:54 -0300

    Initial server

---
+++ server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true }, // Permite conexões do React (CORS)
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Println("WebSocket server started on ws://localhost:8081/ws")
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
