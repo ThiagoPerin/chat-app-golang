f54eab45d39cff5740ae071cbf66805c8c91ec6b
Thiago Perin <thiagoperin2310@gmail.com>
2025-04-08 01:37:17 -0300

    frontend: improves ui and code structure from main page

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,56 +1,33 @@
 "use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation"
+import { ModeToggle } from "@/components/custom/button/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { useEffect, useState } from "react";
-import { ModeToggle } from "@/components/custom/button/ModeToggle";
+import { Card, CardContent } from "@/components/ui/card";
 
 const ChatApp = () => {
+  const router = useRouter()
   const [username, setUsername] = useState("");
-  
+
   function handleEnterChat(e: { preventDefault: () => void; }) {
     e.preventDefault();
     localStorage.setItem("username", username);
-    window.location.href = "/livechat";
+    router.push("/livechat");
   }
 
-  // useEffect(() => {
-  //   const storedUsername = localStorage.getItem("username");
-  //   if (storedUsername) {
-  //     setUsername(storedUsername);
-  //   }
-  // }, [])
-
   return (
-    <div className="h-screen w-screen relative flex items-center justify-center bg-white dark:bg-black p-4">
-      <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Live Chat App</CardTitle>
-        <CardDescription>A simple live chat app</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={handleEnterChat}>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" placeholder="Type your username..." value={username} onInput={(e) => setUsername(e.currentTarget.value)} required />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button className="w-full" onClick={handleEnterChat}>Enter Chat</Button>
-      </CardFooter>
-    </Card>
-        <ModeToggle />
+    <div className="h-screen w-screen relative flex flex-col items-center justify-center gap-10 p-4">
+      <h1 className="text-8xl font-extralight text-center">Live Chat App</h1>
+      <Card className="w-full max-w-xl">
+        <CardContent>
+          <form onSubmit={handleEnterChat} className="grid w-full gap-3 items-center">
+            <Input id="username" placeholder="Enter your username..." value={username} onInput={(e) => setUsername(e.currentTarget.value)} required />
+            <Button className="w-full" type="submit">Enter Chat</Button>
+          </form>
+        </CardContent>
+      </Card>
+      <ModeToggle />
     </div>
   );
 };
