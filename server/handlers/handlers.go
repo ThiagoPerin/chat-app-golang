5c8f770aca21e54d78177f0cd15744f7e6ad302d
Thiago Perin <thiagoperin2310@gmail.com>
2025-04-08 01:17:50 -0300

    backend: create handler package

---
+++ server/handlers/handlers.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ThiagoPerin/chat-app-golang/models"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true }, // Permite conexões do React (CORS)
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan models.Message)
+
+func HandleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg models.Message
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func HandleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
